7b9eeeff53fe22cd676f9de4c66479c22ab8da7a
Cr Seven <sevencr024@gmail.com>
2025-08-13 10:33:13 +0000

    Let’s imagine a UI made just for someone called “Pookie”:
    Feature	Pooki

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['"PT Sans"', 'sans-serif'],
+        body: ['"Nunito"', 'sans-serif'],
         headline: ['"Playfair Display"', 'serif'],
         code: ['monospace'],
       },
